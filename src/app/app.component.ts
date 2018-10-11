bfe232294ee311853f48fe051ef49bfd08c45225
Juan Felipe Betancur Cano <juanfelipebetancurcano@gmail.com>
2018-10-10 22:49:52 -0500

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,19 +4,34 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { TranslateService } from '@ngx-translate/core';
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, private _translate: TranslateService) {
     platform.ready().then(() => {
+      this._initTranslate();
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  private _initTranslate() {
+    //Seleccionar el lenguaje por defecto
+    this._translate.setDefaultLang('en');
+
+    if (this._translate.getBrowserLang() !== undefined) {
+      //Seleccionar un lenguaje dependiendo de la ubicación
+      this._translate.use(this._translate.getBrowserLang());
+    } else {
+        this._translate.use('en'); //Seleccionar un lenguaje por defecto
+    }
   }
 }
 
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,16 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(private _translate: TranslateService) {
 
+  }
+
+  public ionViewDidLoad() : void {
+    this._translate.use('en');
+  }
+
+  private _translateLanguage(language: string) : void {
+    this._translate.use(language);
   }
 
 }
